1fd33372d760edfa05119152678110e7b10d6cd1
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-08-22 13:46:32 +0530

    Add files via upload

--- openaianswer.py
+++ openaianswer.py
@@ -1,18 +1,20 @@
 import openai
 import streamlit as st
 # from dotenv import load_dotenv
-# import os
+import os
 
 # load_dotenv()
-key=st.secrets['api-key']
+# key=st.secrets['api-key']
+# key=os.environ['API_KEY']
 # key=os.getenv('API_KEY')
-openai.api_key=key
+# openai.api_key='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'
 # openai.api_key='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'
 def create_prompt(context,query):
     header = "Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text and requires some latest information to be updated, print 'Sorry Not Sufficient context to answer query' \n"
     return header + context + "\n\n" + query + "\n"
 
-def generate_answer(prompt):
+def generate_answer(prompt,key):
+    openai.api_key=key
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=prompt,
