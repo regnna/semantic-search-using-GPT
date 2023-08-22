c77ae91db9e43a779fc075ba047c1c0353cacdd1
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-08-22 13:46:02 +0530

    Add files via upload

--- vector_search.py
+++ vector_search.py
@@ -1,18 +1,20 @@
 # from dotenv import load_dotenv
-# import os
+import os
 import streamlit as st
-# key=os.getenv('PINECONE_API')
+key=os.getenv('PINECONE_API')
 # load_dotenv()
-key=st.secrets["PINECONE_API"]
+# key=st.secrets["PINECONE_API"]
 import pinecone
 from sentence_transformers import SentenceTransformer,util
 # from sentence_transformers import SentenceTransformer,util
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-pinecone.init(api_key=key,environment="northamerica-northeast1-gcp")
-index=pinecone.Index("indi")
+pinecone.init(api_key='b4367181-aa10-4642-9a9d-978b5f586928',environment="northamerica-northeast1-gcp")
+index=pinecone.Index("indie")
+
 
 def addData(corpusData,url):
+    
     id  = index.describe_index_stats()['total_vector_count']
     for i in range(len(corpusData)):
         chunk=corpusData[i]
@@ -21,7 +23,8 @@ def addData(corpusData,url):
                 {'title': url,'context': chunk})
         index.upsert(vectors=[chunkInfo])
 
-def find_match(query,k):
+def find_match(query,k,):
+    
     query_em = model.encode(query).tolist()
     result = index.query(query_em, top_k=k, includeMetadata=True)
     
